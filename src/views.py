50d9c8a66173852990cd8ea4e68ae787c3e80e58
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2023-11-11 22:14:34 +0100

    feat: Add exit button, render settings button

--- src/views.py
+++ src/views.py
@@ -89,23 +89,33 @@ class Menu(View):
 
         self.fonts = FontManager()
         self.title = self.fonts.font_title.render("Traffic Evader", True, "black")
-        self.play_button = Button((WIDTH // 2 - 75, HEIGHT - 350, 150, 50), text="Play")
+
+        self.play = Button((WIDTH // 2 - 75, HEIGHT - 350, 150, 50), text="Play")
+        self.settings = Button((WIDTH // 2 - 75, HEIGHT - 290, 150, 50), text="Settings")
+        self.exit_btn = Button((WIDTH // 2 - 75, HEIGHT - 230, 150, 50), text="Quit")
+
+        self.buttons = [self.play, self.settings, self.exit_btn]
 
     def process_input(self) -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                self.play_button.click_event()
+                for button in self.buttons:
+                    button.click_event()
 
-        if self.play_button.clicked:
+        if self.play.clicked:
             self.active = False
             self.transition_to = Game()
+
+        if self.exit_btn.clicked:
+            self.exit()
 
     def render(self) -> None:
         self.screen.fill((255, 255, 255))
 
-        self.play_button.draw(self.screen)
+        for button in self.buttons:
+            button.draw(self.screen)
 
         self.screen.blit(self.title, ((WIDTH - self.title.get_width()) // 2, HEIGHT - 450))
 
