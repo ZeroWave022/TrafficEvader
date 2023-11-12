41be8abbca8ab625cc2b25635e82fec2ce820d1b
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2023-11-12 14:12:29 +0100

    refactor: Remove unused code

--- src/other.py
+++
@@ -1,6 +0,0 @@
-
-class GameState():
-    """Class managing game states"""
-    def __init__(self) -> None:
-        """Set initial values for state variables needed globally in Game class"""
-        self.game_on = True
